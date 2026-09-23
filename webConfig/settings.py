c2d42f98a8a9a44c4d6671052c95f44f1c98b306
iqboljon <iqbol@gmail.com>
2026-09-23 18:58:18 +0500

    new

--- webConfig/settings.py
+++ webConfig/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(1muf(y338+6sftpodwvas6q5&ifr!s*+wm-=(%s%)l03rc_r)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
