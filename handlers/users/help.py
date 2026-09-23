5c6c5b3bb80a7eaa9ad09147e9fff797128aeef2
iqboljon <iqbol@gmail.com>
2026-09-23 20:09:38 +0500

    aa

--- handlers/users/help.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Router, filters, types
-rt = Router()
-@rt.message(filters.Command("help"))
-async def help(msg: types.Message):
-    n = "bizda bor barcha kamandalar:\n"
-    n += "/button\n"
-    n += "/tugma\n"
-    await msg.answer(n)
