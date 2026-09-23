4ee58e7a9bb1e932354ee96151b7e584ffd0265e
iqboljon <iqbol@gmail.com>
2026-09-23 20:01:11 +0500

    aa

--- webApp/views.py
+++ webApp/views.py
@@ -1,8 +1,9 @@
-import json
+import json, asyncio
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from aiogram import Bot, Dispatcher, types
-import asyncio
+from aiogram.client.session.aiohttp import AiohttpSession
+
 from .handlers.users.start import rt as start
 from .handlers.users.help import rt as help
 from .handlers.users.button import rt as button
@@ -10,18 +11,22 @@ from .handlers.users.tugma import rt as tugma
 
 TOKEN = "8863118900:AAH8NlAS7IqMf5Q4GSrDz2bUKAxdJJRP_Ak"
 PROXY = "http://proxy.server:3128"
-dp = Dispatcher()
-bot = Bot(token=TOKEN)
 
-dp.include_router(start)
-dp.include_router(help)
-dp.include_router(button)
-dp.include_router(tugma)
+dp = Dispatcher()
+dp.include_routers(start, help, button, tugma)  # Barchasini bitta qatorda ulash mumkin
+
 
 @csrf_exempt
 def telegram_webhook(request):
     if request.method == 'POST':
-        update = types.Update(**json.loads(request.body))
-        asyncio.run(dp.feed_update(bot, update))
+        async def process():
+            # Har bir so'rov uchun alohida sessiya va bot ochiladi:
+            async with AiohttpSession(proxy=PROXY) as session:
+                bot = Bot(token=TOKEN, session=session)
+                update = types.Update(**json.loads(request.body))
+                await dp.feed_update(bot, update)
+
+        asyncio.run(process())
         return HttpResponse("OK")
     return HttpResponse(status=405)
+
