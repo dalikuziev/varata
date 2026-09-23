739512ab3adf5b455baae818e51b13c448fdac97
iqboljon <iqbol@gmail.com>
2026-09-23 19:59:18 +0500

    aa

--- webApp/views.py
+++ webApp/views.py
@@ -1,54 +1,27 @@
 import json
-from django.http import JsonResponse, HttpResponseForbidden
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from aiogram import Bot, Dispatcher, types
-from aiogram.client.session.aiohttp import AiohttpSession
-
-TOKEN = "8863118900:AAH8NlAS7IqMf5Q4GSrDz2bUKAxdJJRP_Ak"
-PROXY = "http://proxy.server:3128"
-dp = Dispatcher()
-
 import asyncio
-from aiogram import Dispatcher, Bot
 from .handlers.users.start import rt as start
 from .handlers.users.help import rt as help
 from .handlers.users.button import rt as button
 from .handlers.users.tugma import rt as tugma
-from aiogram import Router, filters, types
-rt = Router()
-@rt.message(filters.Command("start"))
-async def start(msg: types.Message):
-    await msg.answer("Labbay xo'jayin\nnima yordam beray?")
-    await msg.answer("yordam kerak bo'lsa: /help")
 
+TOKEN = "8863118900:AAH8NlAS7IqMf5Q4GSrDz2bUKAxdJJRP_Ak"
+PROXY = "http://proxy.server:3128"
+dp = Dispatcher()
+bot = Bot(token=TOKEN)
+
+dp.include_router(start)
 dp.include_router(help)
 dp.include_router(button)
 dp.include_router(tugma)
 
-
 @csrf_exempt
 def telegram_webhook(request):
-
     if request.method == 'POST':
-        try:
-            data = json.loads(request.body.decode('utf-8'))
-            update = types.Update(**data)
-
-            async def handle_update():
-                # Har bir so'rov uchun toza sessiya va bot nusxasi:
-                session = AiohttpSession(proxy=PROXY)
-                bot = Bot(token=TOKEN, session=session)
-                try:
-                    await dp.feed_update(bot, update)
-                finally:
-                    await session.close()
-
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            loop.run_until_complete(handle_update())
-            loop.close()
-
-            return JsonResponse({'status': 'ok'})
-        except Exception as e:
-            return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
-    return HttpResponseForbidden()
+        update = types.Update(**json.loads(request.body))
+        asyncio.run(dp.feed_update(bot, update))
+        return HttpResponse("OK")
+    return HttpResponse(status=405)
