375c11e2abc3d2ff641f1f4b2688a56f2e32af15
iqboljon <iqbol@gmail.com>
2026-09-23 20:03:31 +0500

    aa

--- webApp/views.py
+++ webApp/views.py
@@ -1,4 +1,4 @@
-import json, asyncio
+import json, asyncio, threading
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from aiogram import Bot, Dispatcher, types
@@ -13,20 +13,25 @@ TOKEN = "8863118900:AAH8NlAS7IqMf5Q4GSrDz2bUKAxdJJRP_Ak"
 PROXY = "http://proxy.server:3128"
 
 dp = Dispatcher()
-dp.include_routers(start, help, button, tugma)  # Barchasini bitta qatorda ulash mumkin
+dp.include_routers(start, help, button, tugma)
 
+def process_update(data):
+    """Xabarni orqa fonda qayta ishlash"""
+    async def run():
+        async with AiohttpSession(proxy=PROXY) as session:
+            bot = Bot(token=TOKEN, session=session)
+            update = types.Update(**data)
+            await dp.feed_update(bot, update)
+    asyncio.run(run())
 
 @csrf_exempt
 def telegram_webhook(request):
     if request.method == 'POST':
-        async def process():
-            # Har bir so'rov uchun alohida sessiya va bot ochiladi:
-            async with AiohttpSession(proxy=PROXY) as session:
-                bot = Bot(token=TOKEN, session=session)
-                update = types.Update(**json.loads(request.body))
-                await dp.feed_update(bot, update)
-
-        asyncio.run(process())
-        return HttpResponse("OK")
+        try:
+            data = json.loads(request.body)
+            # Telegram kutib qolmasligi uchun fon rejimida ishga tushiramiz:
+            threading.Thread(target=process_update, args=(data,)).start()
+            return HttpResponse("OK")  # Telegramga 0.01 soniyada javob qaytadi
+        except Exception:
+            return HttpResponse("OK")
     return HttpResponse(status=405)
-
